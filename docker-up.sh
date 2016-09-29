d261b3b33e99d96c81316a811f9a629798eb3cda
Brian <bcfuchs@gmail.com>
2016-09-29 10:30:12 +0100

    adding notes...

--- docker-up.sh
+++ docker-up.sh
@@ -1,11 +1,30 @@
 # script to run Redwire with sample data
-# see docker-up-example.sh for params. 
+# expects WS_IP=ip-of-websocket-server WS_PORT=port-of-websocket-server
+# see docker-up-example.sh for example invocation.
 
-IMAGE_NAME=tmc/glassnode
-# build image if nec.
-if [[ "$(docker images -q $IMAGE_NAME   2> /dev/null)" == "" ]]; then
-     docker build -t tmc/glassnode .
+NODEJS_IMAGE_NAME=tmc/glassnode
+WS_IMAGE_NAME=tmc/nodejs-ws
+
+# build nodejs image if nec.
+if [[ "$(docker images -q $NODEJS_IMAGE_NAME   2> /dev/null)" == "" ]]; then
+    echo "building nodejs docker image"
+    docker build -t $NODEJS_IMAGE_NAME  .
+else
+    echo "found a nodejs image..."
 fi
+
+
+# build ws image if nec.
+
+if [[ "$(docker images -q $WS_IMAGE_NAME   2> /dev/null)" == "" ]]; then
+    echo "building websocket server  docker image"
+    docker build -t $WS_IMAGE_NAME  .
+
+else
+        echo "found a websocket server image..."
+fi
+
+
 # start mongo container
 sh bin/mongo.sh
 
