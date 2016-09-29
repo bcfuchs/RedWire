e46ae25a77f2586026ddbc2293c76890babf178f
Brian <bcfuchs@gmail.com>
2016-09-29 10:31:03 +0100

    adding notes...

--- ws_server/run.sh
+++ ws_server/run.sh
@@ -1,3 +1,2 @@
-PORT=8092
-echo "STARTING websocket server on port " $PORT
-docker run -d  --name glass_ws -p $PORT:8092 -v $PWD:/app  tmc/nodejs-ws  ws/ws_server.js
+echo "STARTING websocket server on port " $WS_PORT
+docker run -d  --name glass_ws -p $WS_PORT:8092 -v $PWD:/app  tmc/nodejs-ws  ws/ws_server.js
